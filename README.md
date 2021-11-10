502af34c4197e44f47da827d238aa0858ff3f7d3
sndmaa <sanclaw55@gmail.com>
2021-11-10 13:30:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+There are 2 ways to upload your JSON into the program in order to get the schema. It can be done through CLI or by inputting the relative file path into the prompt when the program is run with python:
+1.) CLI METHOD# python main.py [RELATIVE FILE PATH]
+# python main.py data/data_1.json
+2.) INPUT PROMPT METHOD: After lauching Program you will be asked to "Please Enter File Name i.e data/data_1.json" You just follow that format with the relevant file name and you will find your output in the path specified in the program.
+
+After program has finished running succefully you will get a prompt saying: "Program has been successfully run. Go to schema/schema_1.json to see output." This means that the program has been completed and you can see its output in the schema folder
+
+The program has no extra dependencies. as long as you have python installed the program can run with the dependecies that come natively with python. 
+
+
+
+PS: I did my best to follow the instructions listed in the readme.md but if I missed something for any reason I hope I am marked based on code logic. My code is exactly similar with the example.json file given and I am grateful to have worked for data2bots on this whether or not I get the job, it was a good practice project.
---
+++ main.py
@@ -0,0 +1,95 @@
+import os
+from pathlib import Path
+import json, sys
+
+    
+
+
+def work(file):
+
+    #get file path
+    BASE_DIR = Path(__file__).resolve().parent
+    JSON_DIR = os.path.join(BASE_DIR, 'data/data_1.json')
+
+
+    #Deserialize from file
+    with open(JSON_DIR, 'r', encoding='utf8', newline='') as f:
+        decode_data = json.load(f)
+
+
+
+
+
+    #function to store and dump dictionary data after each loop and fulfilling if/else conditions
+    def storage(k, copy, store={}):
+        store.update({k : copy})
+        jason = json.dumps(store, indent=4)
+        dump_destination = 'schema/schema_1.json'
+
+        #Serialize the JSON file and have it dumped into the dump_destination specified.
+        with open(dump_destination, 'w', encoding='utf-8', newline='') as jw:
+            jw.write(jason)
+        print('Program has been successfully run. Go to schema/schema_1.json to see output.')
+
+    #loop to iterate through python object and get desired data type info
+    for key, value in decode_data["message"].items():
+
+        diction = {}
+        if (type(value)) == dict:
+            diction.update({'type': 'Object'})
+            diction.update({'tag':''})
+            diction.update({'description':''})
+            diction.update({'required':False})
+
+            storage(key, diction)
+
+        elif (type(value)) == str:
+            str.update({'type': 'String'})
+            diction.update({'tag':''})
+            diction.update({'description':''})
+            diction.update({'required':False})
+
+            storage(key, diction)
+
+        elif (type(value)) == int:
+            diction.update({'type': 'Integer'})
+            diction.update({'tag':''})
+            diction.update({'description':''})
+            diction.update({'required':False})
+
+            storage(key, diction)
+
+        elif (type(value)) == list:
+            diction.update({'type': 'Array/List'})
+            diction.update({'tag':''})
+            diction.update({'description':''})
+            diction.update({'required':False})
+
+            storage(key, diction)
+
+        elif (type(value)) == bool:
+            diction.update({'type': 'Boolean'})
+            diction.update({'tag':''})
+            diction.update({'description':''})
+            diction.update({'required':False})
+
+            storage(key, diction)
+
+        else:
+            print('Not any of these!')
+
+if __name__ == "__main__":
+    print('Welcome to the Chike JSON Schema Coverter made for Data2Bots Assessment. Please enter the JSON relative file path')
+    file_name = ""
+
+    ## test if file name given in command line
+    if len(sys.argv) == 2:
+		## grab from command line
+	    file_name = sys.argv[1]
+    else:
+		## Get file from user
+	    file_name = input("Please enter file name i.e data/data_1.json: ")
+    
+    work(file_name)
+
+
